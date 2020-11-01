cdcdb05a02cc53ca5805a1907905986d8fd20583
Youngjun Lee <youngjle@c8r8s8.42seoul.kr>
2020-11-01 23:20:31 +0900

    add ex00 ex01 ex02 try ex03

---
+++ ex02/ft_putnbr.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: youngjle <youngjle@student.42seoul.>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/01 03:53:20 by youngjle          #+#    #+#             */
+/*   Updated: 2020/11/01 18:52:11 by youngjle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		ft_putnbr(nb / 10);
+		ft_putchar('8');
+	}
+	else if (nb < 0)
+	{
+		ft_putchar('-');
+		ft_putnbr(-nb);
+	}
+	else
+	{
+		if (nb > 9)
+		ft_putnbr(nb / 10);
+		ft_putchar(48 + nb % 10);
+	}
+
+}
